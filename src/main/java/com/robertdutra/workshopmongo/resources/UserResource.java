e0d76874b0c923bcca0962d446468dd48bd0703a
Robert Dutra <robertdutra10@hotmail.com>
2023-06-26 19:35:54 -0300

    Obtendo um usuário por id

--- src/main/java/com/robertdutra/workshopmongo/resources/UserResource.java
+++ src/main/java/com/robertdutra/workshopmongo/resources/UserResource.java
@@ -1,13 +1,13 @@
 package com.robertdutra.workshopmongo.resources;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.robertdutra.workshopmongo.domain.User;
@@ -21,10 +21,16 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 
-	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping
 	public ResponseEntity<List<UserDTO>> findAall() {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(user));
 	}
 }
--- src/main/java/com/robertdutra/workshopmongo/service/UserService.java
+++ src/main/java/com/robertdutra/workshopmongo/service/UserService.java
@@ -1,12 +1,14 @@
 package com.robertdutra.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.robertdutra.workshopmongo.domain.User;
 import com.robertdutra.workshopmongo.repository.UserRepository;
+import com.robertdutra.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
 }
