e36644b848e3fae8f3ebcfe010ef67aba2fa866a
Robert Dutra <robertdutra10@hotmail.com>
2023-06-27 18:10:02 -0300

    Consulta simples com query methods

--- src/main/java/com/robertdutra/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/robertdutra/workshopmongo/repository/PostRepository.java
@@ -1,9 +1,12 @@
 package com.robertdutra.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.robertdutra.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
-
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
