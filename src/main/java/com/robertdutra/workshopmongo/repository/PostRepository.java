1b1e8b064f7e8f85392431726aba8c0654977966
Robert Dutra <robertdutra10@hotmail.com>
2023-06-27 18:15:47 -0300

    Consulta simples com @Query

--- src/main/java/com/robertdutra/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/robertdutra/workshopmongo/repository/PostRepository.java
@@ -3,10 +3,15 @@ package com.robertdutra.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.robertdutra.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
 	
+	@Query("{ 'title' : { $regex: ?0, $options: 'i'} }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
 }
